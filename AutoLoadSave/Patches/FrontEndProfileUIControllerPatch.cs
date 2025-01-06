5be196bf313bd7a45d8f863fb60dbc392ff6ba4a
86maylin <86maylin@gmail.com>
2025-01-06 15:03:04 +0800

    Initial commit

---
+++ AutoLoadSave/Patches/FrontEndProfileUIControllerPatch.cs
@@ -0,0 +1,74 @@
+﻿using BBI.Unity.Game;
+using HarmonyLib;
+using System.Collections;
+using UnityEngine;
+
+namespace AutoLoadSave.Patches
+{
+    [HarmonyPatch(typeof(FrontEndProfileUIController))]
+    internal class FrontEndProfileUIControllerPatch
+    {
+        [HarmonyPatch("SetupProfileButtons")]
+        [HarmonyPostfix]
+        static void SetupProfileButtons_Postfix(FrontEndProfileUIController __instance)
+        {
+            AutoLoadSave.LoggerInstance.LogInfo("SetupProfileButtons_Postfix");
+            if (AutoLoadSave.HasLoadedSave) return;
+            AutoLoadSave.Instance.StartCoroutine(SelectProfileButton(__instance));
+        }
+
+        static IEnumerator SelectProfileButton(FrontEndProfileUIController instance)
+        {
+            AutoLoadSave.LoggerInstance.LogInfo("SetupProfileButtons");
+            yield return null;
+            string buttonName;
+            switch (AutoLoadSave.Config_shiftType.Value)
+            {
+                case (int)AutoLoadSave.SaveType.OpenShift:
+                    buttonName = "Open Shift Button";
+                    break;
+                case (int)AutoLoadSave.SaveType.Limited:
+                    buttonName = "Hard Button";
+                    break;
+                case (int)AutoLoadSave.SaveType.NoRevival:
+                    buttonName = "Extreme Button";
+                    break;
+                case (int)AutoLoadSave.SaveType.Standard:
+                default:
+                    buttonName = "Normal Button";
+                    break;
+            }
+            foreach (var button in Object.FindObjectsOfType<FrontEndProfileButton>(true))
+            {
+                if (button.name == buttonName)
+                {
+                    if (button.PlayerProfile == null)
+                    {
+                        AutoLoadSave.LoggerInstance.LogInfo("Playerprofile empty, can't load save.");
+                        AutoLoadSave.HasLoadedSave = true;
+                        yield break;
+                    }
+                    instance.CurrentlySelectedProfileButton = button;
+                    break;
+                }
+            }
+            if (AutoLoadSave.Config_shiftType.Value == (int)AutoLoadSave.SaveType.OpenShift)
+            {
+                instance.GoToNext(AutoLoadSave.Config_drainDisabled.Value);
+            }
+            else
+            {
+                instance.GoToNext(false);
+            }
+        }
+
+        [HarmonyPatch(nameof(FrontEndProfileUIController.GoToNext))]
+        [HarmonyPostfix]
+        static void GoToNext_Postfix()
+        {
+            if (AutoLoadSave.HasLoadedSave) return;
+            SceneLoader.Instance.TearDownAndLoadLevelWithPlayerProfile(PlayerProfileService.Instance.Profile.ProfileName, true);
+            AutoLoadSave.HasLoadedSave = true;
+        }
+    }
+}
---
+++ AutoLoadSave/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace AutoLoadSave
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class AutoLoadSave : BaseUnityPlugin
+    {
+        public enum SaveType
+        {
+            OpenShift, Standard, Limited, NoRevival
+        }
+
+        private const string modGUID = "86maylin.auto_load_save";
+        private const string modName = "Automatically load career save when starting the game";
+        private const string modVersion = "1.0.0.0";
+
+        public static AutoLoadSave Instance { get; private set; }
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static ManualLogSource LoggerInstance;
+        public static ConfigEntry<bool> Config_enabled { get; private set; }
+        public static ConfigEntry<int> Config_shiftType { get; private set; }
+        public static ConfigEntry<bool> Config_drainDisabled { get; private set; }
+        public static bool HasLoadedSave = false;
+
+        void Awake()
+        {
+            Instance = this;
+            LoggerInstance = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            SetConfigs();
+            if (Config_enabled.Value)
+            {
+                harmony.PatchAll();
+                LoggerInstance.LogInfo("AutoLoadSave patched.");
+            }
+            else
+            {
+                LoggerInstance.LogInfo("AutoLoadSave disabled.");
+            }
+        }
+
+        private void SetConfigs()
+        {
+            Config_enabled = Config.Bind("Config", "Enabled", true, "Whether to enable the plugin or not.");
+            Config_shiftType = Config.Bind("Config", "ShiftType", 1, "Shift type. 0: OpenShift 1: Standard 2: Limited 3: No Revival");
+            Config_drainDisabled = Config.Bind("Config", "DrainDisabled", false, "Whether to disable oxygen drain for OpenShift mode or not.");
+        }
+    }
+}
